318d24e32db12685e4499146c996144b086961c3
mignonat <mignonat@hotmail.com>
2017-05-28 19:14:57 +0200

    users admin page

--- src/client/components/contents/admin/Users.vue
+++ src/client/components/contents/admin/Users.vue
@@ -1,3 +1,18 @@
 <template>
     <div class="content">Page d'administration des utilisateurs</div>
 </template>
+
+<script>
+    export default {
+        data : function() { return {
+            loading : true,
+            users : []
+        }},
+        methods : {},
+        mounted: function () {
+            this.$nextTick(function () {
+                
+            })
+        }
+    }
+</script>
