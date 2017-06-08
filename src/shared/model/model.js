93786816e0ee3e9e4d12a23e8e22f511298876f1
Mignonat Laurent <laurent.mignonat@nqicorp.com>
2017-06-08 17:15:24 +0200

    entities model

--- src/shared/model/model.js
+++ src/shared/model/model.js
@@ -36,11 +36,11 @@ const fieldTypes = {
     object  : { fieldType : nativeTypes.object },
     locale  : {
         fieldType : nativeTypes.string,
-        regex : new RegExp(/([f][r])|([e][n])/ig)
+        regex : new RegExp(/(^[f][r]$)|(^[e][n]$)/i)
     },
     date : {
         fieldType : nativeTypes.string,
-        regex : new RegExp(/\d{4}-[01]\d-[0-3]\dT[0-2]\d/ig)
+        regex : new RegExp(/^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/)
     },
     datetime : {
         fieldType : nativeTypes.string,
