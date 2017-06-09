a349e75abf55e45f37d4ad51b1689c874f2d20cc
Mignonat Laurent <laurent.mignonat@nqicorp.com>
2017-06-09 18:27:24 +0200

    rename 2 css files

--- src/client/app.js
+++ src/client/app.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import router from './router'
 import store from './vuex/store'
 import App from './component/App.vue'
-import css from './resource/css/main.css' // managed by webpack
+import css from './resource/css/index.css' // managed by webpack
 
 // Route components will be rendered inside <router-view>
 export default new Vue({
