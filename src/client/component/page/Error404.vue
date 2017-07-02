b5cac0070c743360b3edaa848dd93597c58933e2
Florian Bonacina <florian.bonacina@gmail.com>
2017-07-02 16:07:46 +0200

    work on 404 page

--- src/client/component/page/Error404.vue
+++ src/client/component/page/Error404.vue
@@ -1,12 +1,66 @@
 <template>
-    <div class="page-content error-content">
-        <div class='landscape'>
-            <div class="stars"></div>
-            <div class="twinkling"></div>
-            <div class="clouds"></div>
-            <div class='moon'></div>
-            <div class='layer l1'>
-                <div class='hills'>
+    <div id="page404-error-content" class="page404-content page404-error-content aurora-borealis">
+        <div id="page404-sky" class='page404-landscape'>
+            <div id="page404-sky-aurora" style="height: 100%;z-index: 150;" class="page404-sky-aurora-hidden aurora-borealis"></div>
+            <div id="page404-stars" class="page404-stars"></div>
+            <div id="page404-twinkling" class="page404-twinkling"></div>
+            <div id="page404-clouds" class="page404-clouds"></div>
+             <div id="page404-sun" class="page404-sun page404-sun-hidden">
+                <div class="page404-sun-anime">
+                    <div class="page404-sun-ball"></div>
+                    <div class="page404-sun-light">
+                        <b></b>
+                        <s></s>
+                    </div>
+                    <div class="page404-sun-light">
+                        <b></b>
+                        <s></s>
+                    </div>
+                    <div class="page404-sun-light">
+                        <b></b>
+                        <s></s>
+                    </div>
+                    <div class="page404-sun-light">
+                        <b></b>
+                        <s></s>
+                    </div>
+                    <div class="page404-sun-light">
+                        <b></b>
+                        <s></s>
+                    </div>
+                    <div class="page404-sun-light">
+                        <b></b>
+                        <s></s>
+                    </div>
+                    <div class="page404-sun-light">
+                        <b></b>
+                        <s></s>
+                    </div>
+                    <div class="page404-sun-light">
+                        <b></b>
+                        <s></s>
+                    </div>
+                    <div class="page404-sun-light">
+                        <b></b>
+                        <s></s>
+                    </div>
+                    <div class="page404-sun-light">
+                        <b></b>
+                        <s></s>
+                    </div>
+                    <div class="page404-sun-light">
+                        <b></b>
+                        <s></s>
+                    </div>
+                    <div class="page404-sun-light">
+                        <b></b>
+                        <s></s>
+                    </div>
+                </div>
+            </div>
+            <div id="page404-moon" class='page404-moon'></div>
+            <div id="page404-line-hills1" class='page404-layer l1'>
+                <div class='page404-hills'>
                 <div></div>
                 <div></div>
                 <div></div>
@@ -17,8 +71,8 @@
                 <div></div>
                 </div>
             </div>
-            <div class='layer l2'>
-                <div class='hills'>
+            <div id="page404-line-hills2" class='page404-layer l2'>
+                <div class='page404-hills'>
                 <div></div>
                 <div></div>
                 <div></div>
@@ -29,9 +83,9 @@
                 <div></div>
                 </div>
             </div>
-            <div class='layer l3'>
-                <div class="containerWaves">
-                    <div class="row-one">
+            <div class='page404-layer l3'>
+                <div id="page404-container-waves" class="page404-containerWaves">
+                    <div id="page404-row-one" class="page404-row-one">
                         <div class="v-wave-l"></div>
                         <div class="wave"></div>
                         <div class="wave"></div>
@@ -111,7 +165,7 @@
                         <div class="wave"></div>
                         <div class="wave"></div>
                     </div>
-                    <div class="row-two">
+                    <div class="page404-row-two">
                         <div class="v-wave-l"></div>
                         <div class="wave"></div>
                         <div class="wave"></div>
@@ -188,16 +242,23 @@
                         <div class="wave"></div>
                         <div class="wave"></div>
                         <div class="wave"></div>
-                        <div class="v-wave-r"></div>
+                        <div class="c"></div>
                     </div>
-                    <div class="outer2">
-                        <div class="outer">
-                            <div class="inner">
-                                <img id="boat"src="https://bit.ly/2bgMEJM">
+                    <div class="page404-boat1">
+                        <div class="boat-outer">
+                            <div class="boat-inner">
+                                <img id="page404-boat" src="../../resource/img/drakar1.png">
                             </div>
                         </div>
                     </div>
-                    <div class="row-three">
+                     <div class="page404-boat2">
+                        <div class="boat-outer">
+                            <div class="boat-inner">
+                                <img id="page404-boat2" src="../../resource/img/drakar1.png">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="page404-row-three">
                         <div class="wave"></div>
                         <div class="wave"></div>
                         <div class="wave"></div>
@@ -271,7 +332,7 @@
                         <div class="wave"></div>
                         <div class="v-wave-r"></div>
                     </div>
-                    <div class="row-four">
+                    <div class="page404-row-four">
                         <div class="v-wave-l"></div>
                         <div class="wave"></div>
                         <div class="wave"></div>
@@ -325,7 +386,7 @@
                         <div class="wave"></div>                
                         <div class="v-wave-r"></div>
                     </div>
-                    <div class="row-five">
+                    <div class="page404-row-five">
                         <div class="v-wave-l"></div>
                         <div class="wave"></div>
                         <div class="wave"></div>
@@ -364,12 +425,59 @@
                         <div class="wave"></div>
                         <div class="wave"></div>
                     </div>
-                    <div class="row-six"></div>
+                    <div class="page404-row-six"></div>
                 </div>
             </div>
-            <div class="text-error">404</div>
-            <div class="sub-text-error">Oops, the page doesn't exist</div>
+            <div class="page404-text-error">404</div>
+            <div class="page404-sub-text-error">Oops, the page doesn't exist</div>
         </div>
     </div>
 </template>  
+<script>
+    export default {
+        mounted : function() {
+            this.$nextTick(function() {
+                const hour = ('0'+(new Date()).getHours()).slice(-2);
 
+                if (hour >= 7 && hour<=8) {
+                    document.getElementById("page404-sun").classList.remove("page404-sun-hidden");
+                    document.getElementById("page404-sun").classList.add("page404-sun-aurora");
+                    document.getElementById("page404-stars").classList.add("page404-stars-hidden");
+                    document.getElementById("page404-twinkling").classList.add("page404-stars-hidden");
+                    document.getElementById("page404-clouds").classList.add("page404-clouds-aurora");
+                    document.getElementById("page404-sky").classList.add("page404-landscape-Aurora");
+                    document.getElementById("page404-moon").classList.add("page404-moon-hidden");
+                    document.getElementById("page404-line-hills1").classList.add("page404-hills-aurora");
+                    document.getElementById("page404-line-hills2").classList.add("page404-hills-aurora");
+                    document.getElementById("page404-error-content").classList.add("page404-day");
+                    document.getElementById("page404-row-one").classList.add("page404-waves-aurora");
+                } else if (hour > 8 && hour<=19) {
+                    document.getElementById("page404-sun").classList.remove("page404-sun-hidden");
+                    document.getElementById("page404-sun").classList.add("page404-animate-sun");
+                    document.getElementById("page404-stars").classList.add("page404-stars-hidden");
+                    document.getElementById("page404-twinkling").classList.add("page404-stars-hidden");
+                    document.getElementById("page404-clouds").classList.add("page404-clouds-day");
+                    document.getElementById("page404-sky").classList.add("page404-landscape-day");
+                    document.getElementById("page404-moon").classList.add("page404-moon-hidden");
+                    document.getElementById("page404-line-hills1").classList.add("page404-day");
+                    document.getElementById("page404-line-hills2").classList.add("page404-day");
+                    document.getElementById("page404-error-content").classList.add("page404-day");
+                    document.getElementById("page404-row-one").classList.add("page404-day");
+                } else if (hour > 19 && hour<=21) {
+                    document.getElementById("page404-twinkling").classList.remove("page404-twinkling");
+                    document.getElementById("page404-stars").classList.add("page404-borealis");
+                    document.getElementById("page404-stars").classList.remove("page404-stars-hidden")
+                    document.getElementById("page404-sky-aurora").classList.remove("page404-sky-aurora-hidden");
+                    document.getElementById("page404-clouds").classList.remove("page404-clouds-day");
+                    document.getElementById("page404-sun").classList.add("page404-sun-hidden");
+                    document.getElementById("page404-sky").classList.remove("page404-landscape-day");
+                    document.getElementById("page404-line-hills1").classList.remove("page404-day");
+                    document.getElementById("page404-line-hills2").classList.remove("page404-day");
+                    document.getElementById("page404-error-content").classList.remove("page404-day");
+                    document.getElementById("page404-row-one").classList.remove("page404-day");
+                    document.getElementById("page404-moon").classList.remove("page404-moon-hidden");
+                }
+            })
+        }
+    }
+</script>
